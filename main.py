b92a501c803a46887bf48fa13070880d0a40e754
Lyudmila157 <Lyudmilak20@gmail.com>
2023-10-18 15:17:03 +0300

    New print

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello, World!")
+import requests
+
+response = requests.get("https://playground.learnqa.ru/api/hello")
+print(response.text)
+
+response = requests.get("https://playground.learnqa.ru/api/get_text")
+print(response.text)
+
+# print("Hello, World!")
